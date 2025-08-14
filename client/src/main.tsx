fd4ed0a646a546b2de032f77a1eea8761b5e14e4
bzinkan <zinkan.brian@gmail.com>
2025-08-14 14:40:52 +0000

    cleanup: remove debug overlay

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,15 +3,8 @@ import App from "./App";
 import "./index.css";
 import ErrorBoundary from "./ErrorBoundary";
 
-// TEMP: Disable Service Worker & clear old caches to avoid white-screen from stale assets
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((regs) => {
-    regs.forEach((r) => r.unregister());
-    if ("caches" in window) {
-      caches.keys().then((keys) => keys.forEach((k) => caches.delete(k)));
-    }
-  });
-}
+import { registerSW } from "./sw-register";
+registerSW();
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
